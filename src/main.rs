aff41f021a580b054df83403d70605a499fef875
tison <wander4096@gmail.com>
2022-02-05 23:56:28 +0800

    *: repository settings
    Signed-off-by: tison <wander4096@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
-extern crate core;
+// Copyright 2022 tison <wander4096@gmail.com>.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 use std::{convert::Infallible, net::SocketAddr};
 
@@ -46,7 +58,7 @@ async fn handle_request(req: Request<Body>) -> anyhow::Result<Response<Body>> {
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let make_svc = hyper::service::make_service_fn(|_| async {
-        Ok::<_, Infallible>(hyper::service::service_fn(move |req| handle_request(req)))
+        Ok::<_, Infallible>(hyper::service::service_fn(handle_request))
     });
     let address = SocketAddr::from(([0, 0, 0, 0], 3000));
     let server = Server::bind(&address).serve(make_svc);
